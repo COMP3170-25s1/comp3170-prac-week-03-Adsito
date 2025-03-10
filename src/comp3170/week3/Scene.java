e376c09b0ff9822165cb3c9526e0ff110e070b39
Adsito <43514196+Adsito@users.noreply.github.com>
2025-03-10 19:03:08 +1100

    Week3

--- src/comp3170/week3/Scene.java
+++ src/comp3170/week3/Scene.java
@@ -19,141 +19,120 @@ import comp3170.ShaderLibrary;
 
 public class Scene {
 
-	final private String VERTEX_SHADER = "vertex.glsl";
-	final private String FRAGMENT_SHADER = "fragment.glsl";
+    final private String VERTEX_SHADER = "vertex.glsl";
+    final private String FRAGMENT_SHADER = "fragment.glsl";
 
-	private Vector4f[] vertices;
-	private int vertexBuffer;
-	private int[] indices;
-	private int indexBuffer;
-	private Vector3f[] colours;
-	private int colourBuffer;
+    private Vector4f[] vertices;
+    private int vertexBuffer;
+    private int[] indices;
+    private int indexBuffer;
+    private Vector3f[] colours;
+    private int colourBuffer;
 
-	private Shader shader;
+    private long oldTime = System.currentTimeMillis();
+    private float angle = 90f; // Angle of rotation (radians)
+    private float speed = 0.1f; // Forward speed
+    private float rotationSpeed = 1f; // Rotation speed (angle increment per frame)
+    private Matrix4f transMatrix = new Matrix4f();
+    private Matrix4f rotMatrix = new Matrix4f();
+    private Matrix4f sclMatrix = new Matrix4f();
+    private Matrix4f modelMatrix = new Matrix4f();
+    private Vector3f position = new Vector3f(1.5f, 0f, 0.0f);
+    private Shader shader;
 
-	public Scene() {
+    public Scene() {
 
-		shader = ShaderLibrary.instance.compileShader(VERTEX_SHADER, FRAGMENT_SHADER);
+        shader = ShaderLibrary.instance.compileShader(VERTEX_SHADER, FRAGMENT_SHADER);
 
-		// @formatter:off
-			//          (0,1)
-			//           /|\
-			//          / | \
-			//         /  |  \
-			//        / (0,0) \
-			//       /   / \   \
-			//      /  /     \  \
-			//     / /         \ \		
-			//    //             \\
-			//(-1,-1)           (1,-1)
-			//
-	 		
-		vertices = new Vector4f[] {
-			new Vector4f( 0, 0, 0, 1),
-			new Vector4f( 0, 1, 0, 1),
-			new Vector4f(-1,-1, 0, 1),
-			new Vector4f( 1,-1, 0, 1),
-		};
-			
-			// @formatter:on
-		vertexBuffer = GLBuffers.createBuffer(vertices);
+        // Define the vertices of the shape (representing a simple triangle plane)
+        vertices = new Vector4f[]{
+            new Vector4f(0, 0, 0, 1),
+            new Vector4f(0, 1, 0, 1),
+            new Vector4f(-1, -1, 0, 1),
+            new Vector4f(1, -1, 0, 1),
+        };
 
-		// @formatter:off
-		colours = new Vector3f[] {
-			new Vector3f(1,0,1),	// MAGENTA
-			new Vector3f(1,0,1),	// MAGENTA
-			new Vector3f(1,0,0),	// RED
-			new Vector3f(0,0,1),	// BLUE
-		};
-			// @formatter:on
+        vertexBuffer = GLBuffers.createBuffer(vertices);
 
-		colourBuffer = GLBuffers.createBuffer(colours);
+        // Define the colors of the vertices
+        colours = new Vector3f[]{
+            new Vector3f(1, 0, 1),    // MAGENTA
+            new Vector3f(1, 0, 1),    // MAGENTA
+            new Vector3f(1, 0, 0),    // RED
+            new Vector3f(0, 0, 1),    // BLUE
+        };
 
-		// @formatter:off
-		indices = new int[] {  
-			0, 1, 2, // left triangle
-			0, 1, 3, // right triangle
-			};
-			// @formatter:on
+        colourBuffer = GLBuffers.createBuffer(colours);
 
-		indexBuffer = GLBuffers.createIndexBuffer(indices);
+        // Define the indices for drawing triangles
+        indices = new int[]{
+            0, 1, 2, // left triangle
+            0, 1, 3, // right triangle
+        };
 
-	}
+        indexBuffer = GLBuffers.createIndexBuffer(indices);
+    }
 
-	public void draw() {
-		
-		shader.enable();
-		// set the attributes
-		shader.setAttribute("a_position", vertexBuffer);
-		shader.setAttribute("a_colour", colourBuffer);
+    public void update() {
+        long time = System.currentTimeMillis();
+        float deltaTime = (time - oldTime) / 1000f;
+        oldTime = time;
+        
+        // Update the angle of rotation
+        angle += rotationSpeed * deltaTime;
+        
+        // Calculate the new position based on the angle
+        position.x = (float) Math.cos(angle) * speed;
+        position.y = (float) Math.sin(angle) * speed;
+    }
 
-		// draw using index buffer
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
-		
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-		glDrawElements(GL_TRIANGLES, indices.length, GL_UNSIGNED_INT, 0);
+    public void draw() {
+        shader.enable();
+        update();
+        shader.setAttribute("a_position", vertexBuffer);
+        shader.setAttribute("a_colour", colourBuffer);
+        modelMatrix.identity();
+        
+        translationMatrix(position.x, position.y, transMatrix);
+        rotationMatrix(-angle, rotMatrix);
+        scaleMatrix(0.1f, 0.1f, sclMatrix);
+        modelMatrix.mul(transMatrix)
+                   .mul(rotMatrix)
+                   .mul(sclMatrix);
+        shader.setUniform("u_modelMatrix", modelMatrix);
 
-	}
+        // Bind the index buffer for drawing
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
 
-	/**
-	 * Set the destination matrix to a translation matrix. Note the destination
-	 * matrix must already be allocated.
-	 * 
-	 * @param tx   Offset in the x direction
-	 * @param ty   Offset in the y direction
-	 * @param dest Destination matrix to write into
-	 * @return
-	 */
+        // Set polygon mode and draw the shape
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        glDrawElements(GL_TRIANGLES, indices.length, GL_UNSIGNED_INT, 0);
+    }
 
-	public static Matrix4f translationMatrix(float tx, float ty, Matrix4f dest) {
-		// clear the matrix to the identity matrix
-		dest.identity();
+    public static Matrix4f translationMatrix(float tx, float ty, Matrix4f dest) {
+        dest.identity();
+    	dest.m30(tx);
+        dest.m31(ty);
+        return dest;
+    }
 
-		//     [ 1 0 0 tx ]
-		// T = [ 0 1 0 ty ]
-	    //     [ 0 0 0 0  ]
-		//     [ 0 0 0 1  ]
+    public static Matrix4f rotationMatrix(float angle, Matrix4f dest) {
+    	dest.identity();
+        float cos = (float) Math.cos(angle);
+        float sin = (float) Math.sin(angle);
 
-		// Perform operations on only the x and y values of the T vec. 
-		// Leaves the z value alone, as we are only doing 2D transformations.
-		
-		dest.m30(tx);
-		dest.m31(ty);
+        dest.m00(cos);
+        dest.m01(-sin);
+        dest.m10(sin);
+        dest.m11(cos);
 
-		return dest;
-	}
+        return dest;
+    }
 
-	/**
-	 * Set the destination matrix to a rotation matrix. Note the destination matrix
-	 * must already be allocated.
-	 *
-	 * @param angle Angle of rotation (in radians)
-	 * @param dest  Destination matrix to write into
-	 * @return
-	 */
-
-	public static Matrix4f rotationMatrix(float angle, Matrix4f dest) {
-
-		// TODO: Your code here
-
-		return dest;
-	}
-
-	/**
-	 * Set the destination matrix to a scale matrix. Note the destination matrix
-	 * must already be allocated.
-	 *
-	 * @param sx   Scale factor in x direction
-	 * @param sy   Scale factor in y direction
-	 * @param dest Destination matrix to write into
-	 * @return
-	 */
-
-	public static Matrix4f scaleMatrix(float sx, float sy, Matrix4f dest) {
-
-		// TODO: Your code here
-
-		return dest;
-	}
-
+    public static Matrix4f scaleMatrix(float sx, float sy, Matrix4f dest) {
+    	dest.identity();
+        dest.m00(sx);
+        dest.m11(sy);
+        return dest;
+    }
 }
